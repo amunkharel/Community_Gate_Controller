7a13248d8af1c2535dad059e5b42bec8df895b95
Brandon Stringham <thebpower@unm.edu>
2021-03-09 17:47:05 +0000

    Created the KeypadManager logic
    Created the KeypadManager logic with some adjustments in anticipation of the exceptions that code scanner and code validator will throw.

--- src/cgk/KeypadManager.java
+++ src/cgk/KeypadManager.java
@@ -1,23 +1,76 @@
 package cgk;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.concurrent.TimeoutException;
 
-public class KeypadManager extends Application {
+import static java.lang.Thread.sleep;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("B-STACK CGK");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+/**
+ * The main controller class for the CGK system.
+ *
+ * This class boots up the system, and then starts up the code scanner waiting
+ * for new code entries. Upon a successful code entry, the manager sends the
+ * code to CodeValidation to check if the code is a gate code or admin code.
+ * If it is a correct code, the manager tells the gate controller to tell the
+ * gate to open and then starts waiting for a new code input. If the code was
+ * invalid, it will notify the user and ask for a new code.
+ */
+public class KeypadManager {
+    private static CodeScanner codeScanner;
+    private static OutputScreenOperator outputScreenOperator;
+    private static CodeVerifier codeVerifier;
+    private static GateCommunicator gateCommunicator;
 
     public static void main(String[] args) {
-        launch(args);
+        // Spin up the sub-components
+        outputScreenOperator = new OutputScreenOperator();
+        codeScanner = new CodeScanner(outputScreenOperator);
+        codeVerifier = new CodeVerifier(outputScreenOperator, codeScanner);
+        gateCommunicator = new GateCommunicator();
+
+        // Start the main state loop
+        processingLoop();
+    }
+
+    /**
+     * This method represents the state machine that is the KeypadManager and
+     * manages the states and transitions between states.
+     */
+    private static void processingLoop(){
+        int enteredCode;
+        int invalidCodeAttempts = 0;
+        CodeVerifier.Responses entryStatus;
+        while(true){
+            if(invalidCodeAttempts >= 5){
+                outputScreenOperator.print("Cooldown");
+                try{
+                    sleep(10 * 1000);
+                } catch(InterruptedException ignored){}
+            }
+            outputScreenOperator.print("Enter Code");
+            try{
+                enteredCode = codeScanner.scanCode(4);
+                entryStatus = codeVerifier.verifyCode(enteredCode);
+            } catch(TimeoutException e) {
+                // If there was a time out, either in the code scanner or the
+                // admin menu, go back to the idle state.
+                outputScreenOperator.print("Idle Timeout");
+                continue;
+            }
+            switch(entryStatus){
+                case VALID:
+                    invalidCodeAttempts = 0;
+                    outputScreenOperator.print("Welcome");
+                    gateCommunicator.openGate();
+                    break;
+                case ADMIN:
+                    invalidCodeAttempts = 0;
+                    // Do not need to handle the admin stuff, CodeManagement already handled it
+                    break;
+                case INVALID:
+                    invalidCodeAttempts++;
+                    outputScreenOperator.print("INVALID CODE\n\n");
+                    break;
+            }
+        }
     }
 }
