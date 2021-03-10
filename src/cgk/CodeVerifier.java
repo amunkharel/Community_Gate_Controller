2cd1495ead9f6f8b45a0c028bd28d1b876e00afb
Cody Crane <cranec@cs.unm.edu>
2021-03-10 14:33:33 -0700

    CodeVerifier V1.0

--- src/cgk/CodeVerifier.java
+++ src/cgk/CodeVerifier.java
@@ -1,10 +1,31 @@
 package cgk;
 
+import java.util.concurrent.TimeoutException;
+
 public class CodeVerifier{
     public enum Responses{
         VALID,
         INVALID,
         ADMIN;
     }
-    public Responses verifyCode(int code){}
+
+    private CodeStorageManager codeStorageManager;
+    private CodeManager codeManager;
+
+    public CodeVerifier(OutputScreenOperator oso, CodeScanner cs) {
+        codeStorageManager = new CodeStorageManager();
+        codeManager = new CodeManager(cs, oso, codeStorageManager);
+    }
+
+    public Responses verifyCode(int code) throws TimeoutException {
+        if (code == codeStorageManager.getCurrentCode(CodeStorageManager.CodeType.ADMIN)) {
+            codeManager.handleAdmin();
+            return Responses.ADMIN;
+        } else if (code == codeStorageManager.getCurrentCode(CodeStorageManager.CodeType.GENERAL) ||
+                   code == codeStorageManager.getCurrentCode(CodeStorageManager.CodeType.PUBLIC_SERVICE)) {
+            return Responses.VALID;
+        } else {
+            return Responses.INVALID;
+        }
+    }
 }
